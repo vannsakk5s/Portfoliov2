4e3f0d1ab02903f919001fab106e390b601ec47a
zakk125 <vannzakk@gmail.com>
2026-04-23 15:49:59 +0700

    last t

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
@@ -10,7 +9,6 @@ import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     Header,
     Hero,
     About,
