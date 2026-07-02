4a038ae11436fead448c0e201d8339d7b0649ec7
zakk125 <vannzakk@gmail.com>
2026-07-02 15:19:20 +0700

    add

--- src/app/components/project-card/project-card.ts
+++ src/app/components/project-card/project-card.ts
@@ -10,6 +10,29 @@ export class ProjectCard {
   title = input.required<string>();
   description = input.required<string>();
   tags = input.required<string[]>();
-  demoLink = input<string>('#');
+  demoLink = input<string>('');
   githubLink = input<string>('#');
+
+  showPopup = false;
+  popupTimeout: any;
+
+  onDemoClick(event: Event) {
+    if (!this.demoLink() || this.demoLink() === '#') {
+      event.preventDefault();
+      this.showPopup = true;
+      if (this.popupTimeout) {
+        clearTimeout(this.popupTimeout);
+      }
+      this.popupTimeout = setTimeout(() => {
+        this.showPopup = false;
+      }, 3000);
+    }
+  }
+
+  closePopup() {
+    this.showPopup = false;
+    if (this.popupTimeout) {
+      clearTimeout(this.popupTimeout);
+    }
+  }
 }
